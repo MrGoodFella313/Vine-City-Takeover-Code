2ef4cdeb0bbe821279f68fb64c55c0280682416e
Hutch-5 <hutchersontarell@gmail.com>
2025-07-22 11:32:37 -0700

    Player and enemy follow still working on

--- Testing/Tarell/enemy.gd
+++ Testing/Tarell/enemy.gd
@@ -2,9 +2,9 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
-@export var player : Node2D 
+@export var player : Node2D
 
-func _physics_process(delta: float) -> void:
+func _physics_process(Delta: float)-> void:
 	#where is the player 
 	var direction = (player.position - position).normalized()
 	velocity = direction * SPEED
